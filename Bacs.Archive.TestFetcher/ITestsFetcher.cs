566ac80fdecae19927a522fac7aa47a073b4d960
Mikhail Ivchenko <ematirov@gmail.com>
2017-12-07 17:33:52 +0400

    Initial release

---
+++ Bacs.Archive.TestFetcher/ITestsFetcher.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Bacs.Archive.TestFetcher
+{
+    public interface ITestsFetcher
+    {
+        IEnumerable<Test> FetchTests(IEnumerable<byte> problemArchive, string problemId, params string[] testsId);
+    }
+}
---
+++ Bacs.Archive.TestFetcher/Test.cs
@@ -0,0 +1,9 @@
+﻿namespace Bacs.Archive.TestFetcher
+{
+    public class Test
+    {
+        public string Id { get; set; }
+        public string Input { get; set; }
+        public string Output { get; set; }
+    }
+}
